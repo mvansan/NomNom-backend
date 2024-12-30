f0962d9ef496e343eaa810a1928b92c7ec3e82c5
unknown <luongquangcbvn@gmail.com>
2024-12-30 17:23:34 +0700

    fix/api

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,9 +1,12 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Tạo interface tùy chỉnh cho req
 interface CustomRequest extends Request {
-  userId?: number;
+  userId?: string;
 }
 
 // Middleware để xác thực token
@@ -12,24 +15,26 @@ export function authenticateToken(
   res: Response,
   next: NextFunction
 ): void {
-  // Lấy token từ cookie hoặc header Authorization
-  const token = req.cookies.token || req.header("Authorization")?.split(" ")[1];
-
+  // Lấy token từ header Authorization
+  const token = req.header("Authorization")?.split(" ")[1];
   if (!token) {
     res.status(401).json({ error: "Access Denied" });
     return;
   }
 
   try {
-    // Giải mã token
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET || "default_secret"
-    ) as { userId: number };
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+
+    if (!decoded.userId) {
+      res.status(400).json({ error: "User ID not found in token" });
+      return;
+    }
+
     req.userId = decoded.userId; // Lưu userId vào req
     next(); // Tiếp tục xử lý request
   } catch (error) {
-    console.error("Token verification error:", error);
     res.status(403).json({ error: "Invalid or expired token" });
   }
 }

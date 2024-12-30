84ced04575a5aece6ad38c90633db950e892b1a3
unknown <luongquangcbvn@gmail.com>
2024-12-30 18:19:26 +0700

    fix/api

--- backend/src/routes/order/order.ts
+++ backend/src/routes/order/order.ts
@@ -10,7 +10,7 @@ import { authenticateToken } from "../../middleware/authMiddleware";
 
 const router = Router();
 
-router.route("/:user_id").get(authenticateToken, getOrders);
+router.route("/").get(authenticateToken, getOrders);
 router.route("/place").post(authenticateToken, placeOrders);
 router.route("/confirm").post(authenticateToken, confirmOrder);
 router.route("/history/:user_id").get(authenticateToken, getOrdersHistory);
